57638d84f5451ce7362bd176cd2b07bb5c5e1937
brunogrighes <brunogrighes>
2024-05-02 01:44:14 -0300

    API category1

--- backend/api/user.js
+++ backend/api/user.js
@@ -52,5 +52,15 @@ module.exports = app => {
                   .catch(err => res.status(500).send(err))
       }
 
+      const getById = (req, res) => {
+            app.db('users')
+                .select('id', 'name', 'email', 'admin')
+                .where({ id: req.params.id })
+                .whereNull('deletedAt')
+                .first()
+                .then(user => res.json(user))
+                .catch(err => res.status(500).send(err))
+        }
+
       return { save, get }
 }
