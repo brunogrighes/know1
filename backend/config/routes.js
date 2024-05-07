384209c1fdeea87dcc87c4da458c60fc002de58b
brunogrighes <brunogrighes@gmail.com>
2024-05-07 01:22:42 -0300

    api passport

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -4,15 +4,18 @@ module.exports = app => {
       app.post('/validateToken', app.api.auth.validateToken)
 
       app.route('/users')
+      .all(app.config.passport.authenticate())
       .post(app.api.user.save)
       .get(app.api.user.get)
 
       app.route('/user/:id')
+      .all(app.config.passport.authenticate())
       .put(admin(app.api.user.save))
       .get(admin(app.api.user.getById))
       .delete(admin(app.api.user.remove))
 
       app.route('/categories')
+      .all(app.config.passport.authenticate())
       .get(admin(app.api.category.get))
       .post(admin(app.api.category.save))
 
@@ -22,15 +25,18 @@ module.exports = app => {
       .get(app.api.category.getTree)
 
       app.route('/categories/:id')
+      .all(app.config.passport.authenticate())
       .get(app.api.category.getById)
       .put(admin(app.api.category.save))
       .delete(admin(app.api.category.remove))
 
       app.route('/articles')
+      .all(app.config.passport.authenticate())
       .get(app.api.article.getById)
       .put(app.api.article.save)
       .delete(app.api.article.remove)
 
       app.route('/categories/:id/articles')
+      .all(app.config.passport.authenticate())
       .get(app.api.article.getByCategory)
 }
