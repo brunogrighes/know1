ee2cf06f567a48a78c96d35a5220d83906b56565
brunogrighes <brunogrighes@gmail.com>
2024-05-07 00:39:27 -0300

    End articles 2 and queries

--- backend/api/article.js
+++ backend/api/article.js
@@ -54,4 +54,33 @@ module.exports = app => {
         .catch(err => res.status(500).send(err))
     }
 
+    const getById = (req, res) => {
+        app.db('articles')
+            .where({ id: req.params.id })
+            .first()
+            .then(article => {
+                article.content = article.content.toString()
+                return res.json(article)
+            })
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getByCategory = async (req, res) => {
+        const categoryId = req.params.id
+        const page = req.query.page || 1
+        const categories = await app.db.raw(queries.categoriesWithChildren,categoryId)
+        const ids = categories.rows.map(c => c.id)
+
+        app.db({a: 'articles', u:'users'})
+        .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+        .limit (limit).offset(page * limit - limit)
+        .whereRaw('?? = ??', ['u.id', 'a.userId'])
+        .WhereIn('categoryId', ids)
+        .orderBy('a.id', 'desc')
+        .then(articles => res.json(articles))
+        .catch(err => res.status(500).send(err))
+
+    }
+
+    return { save, remove, get, getById, getByCategory}
 }
