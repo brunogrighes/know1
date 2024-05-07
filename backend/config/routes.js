0c9dbeb52ed5430feaf9135e69b9e2816061e2ac
brunogrighes <brunogrighes@gmail.com>
2024-05-06 23:31:36 -0300

    Routes APi category

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -4,5 +4,16 @@ module.exports = app => {
       .get(app.api.user.get)
 
       app.route('/user/:id')
-      .put(app.api.user.save)
+      .put(admin(app.api.user.save))
+      .get(admin(app.api.user.getById))
+      .delete(admin(app.api.user.remove))
+
+      app.route('/categories')
+      .get(admin(app.api.category.get))
+      .post(admin(app.api.category.save))
+
+      app.route('/categories/:id')
+      .get(app.api.category.getById)
+      .put(admin(app.api.category.save))
+      .delete(admin(app.api.category.remove))
 }
