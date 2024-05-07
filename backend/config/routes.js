fae1b517c8ca0c684da062a9b7301382f354441c
brunogrighes <brunogrighes@gmail.com>
2024-05-07 01:13:39 -0300

    autentifcação

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,8 @@
 module.exports = app => {
+      app.post('/signup', app.api.user.save)
+      app.post('/signin', app.api.auth.signin)
+      app.post('/validateToken', app.api.auth.validateToken)
+
       app.route('/users')
       .post(app.api.user.save)
       .get(app.api.user.get)
@@ -12,6 +16,11 @@ module.exports = app => {
       .get(admin(app.api.category.get))
       .post(admin(app.api.category.save))
 
+      //cuidado com ordem! Tem q vir antes de /categories/:id
+      app.route('/categories/tree')
+      .all(app.config.passport.authenticate())
+      .get(app.api.category.getTree)
+
       app.route('/categories/:id')
       .get(app.api.category.getById)
       .put(admin(app.api.category.save))
@@ -21,4 +30,7 @@ module.exports = app => {
       .get(app.api.article.getById)
       .put(app.api.article.save)
       .delete(app.api.article.remove)
+
+      app.route('/categories/:id/articles')
+      .get(app.api.article.getByCategory)
 }
