fd4a857d5f1846219e129497d26d9a92207d960f
brunogrighes <brunogrighes@gmail.com>
2024-05-09 01:59:24 -0300

    Router

---
+++ frontend/src/components/home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="home">
+        <PageTitle icon="fa fa-home" main="Dashboard"
+            sub="Base de Conhecimento" />
+        <div class="stats">
+
+        </div>
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+import Stat from './Stat'
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
+
+export default {
+    name: 'Home',
+    components: { PageTitle, Stat },
+    data: function() {
+        return {
+            stat: {}
+        }
+    },
+    methods: {
+        getStats() {
+            axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
+        }
+    },
+    mounted() {
+        this.getStats()
+    }
+}
+</script>
+
+<style>
+
+</style>
