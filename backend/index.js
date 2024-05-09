8a430eccb95d01e8c46a30f93dde34cb429a31e9
brunogrighes <brunogrighes@gmail.com>
2024-05-08 22:31:58 -0300

    api stats

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
-const app = require('express')()
-const consign = require('consign')
-const db = require ('./config/db')
+const app = require("express")();
+const consign = require("consign");
+const db = require("./config/db");
+const mongoose = require("mongoose");
 
-app.db = db
+require("./config/mongodb");
 
-consing()
-      .then('./config/middlewares.js')
-      .then('./api/validation.js')
-      .then('./api')
-      .then('./config/routes.js')
-      .into(app)
+app.db = db;
+app.mongoose = mongoose;
+
+consign()
+  .include("./config/passport.js")
+  .then("./config/middlewares.js")
+  .then("./api/validation.js")
+  .then("./api")
+  .then("./schedule")
+  .then("./config/routes.js")
+  .into(app);
 
 app.listen(4000, () => {
-      console.log('Backend executando...')
-  })
+  console.log("Backend executando...");
+});
