8fed452236455c417229f5de6196d6da5af2bd61
brunogrighes <brunogrighes@gmail.com>
2024-05-08 22:20:51 -0300

    user admin

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,6 @@
+const admin = require('./admin')
+
+
 module.exports = app => {
       app.post('/signup', app.api.user.save)
       app.post('/signin', app.api.auth.signin)
@@ -5,8 +8,8 @@ module.exports = app => {
 
       app.route('/users')
       .all(app.config.passport.authenticate())
-      .post(app.api.user.save)
-      .get(app.api.user.get)
+      .post(admin(app.api.user.save))
+      .get(admin(app.api.user.get))
 
       app.route('/user/:id')
       .all(app.config.passport.authenticate())
@@ -24,19 +27,25 @@ module.exports = app => {
       .all(app.config.passport.authenticate())
       .get(app.api.category.getTree)
 
-      app.route('/categories/:id')
+  app.route('/categories/:id')
       .all(app.config.passport.authenticate())
       .get(app.api.category.getById)
       .put(admin(app.api.category.save))
       .delete(admin(app.api.category.remove))
 
-      app.route('/articles')
+  app.route('/articles')
+      .all(app.config.passport.authenticate())
+      .get(admin(app.api.article.get))
+      .post(admin(app.api.article.save))
+
+  app.route('/articles/:id')
       .all(app.config.passport.authenticate())
       .get(app.api.article.getById)
-      .put(app.api.article.save)
-      .delete(app.api.article.remove)
+      .put(admin(app.api.article.save))
+      .delete(admin(app.api.article.remove))
 
-      app.route('/categories/:id/articles')
+  app.route('/categories/:id/articles')
       .all(app.config.passport.authenticate())
       .get(app.api.article.getByCategory)
+
 }
