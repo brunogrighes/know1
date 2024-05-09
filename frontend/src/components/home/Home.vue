4035bf26c0cf449280d4bd09cbd807e5d43ffffe
brunogrighes <brunogrighes@gmail.com>
2024-05-09 02:08:18 -0300

    Home e Stat

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -3,7 +3,12 @@
         <PageTitle icon="fa fa-home" main="Dashboard"
             sub="Base de Conhecimento" />
         <div class="stats">
-
+            <Stat title="Categorias" :value="stat.categories"
+                icon="fa fa-folder" color="#d54d50" />
+            <Stat title="Artigos" :value="stat.articles"
+                icon="fa fa-file" color="#3bc480" />
+            <Stat title="Usuários" :value="stat.users"
+                icon="fa fa-user" color="#3282cd" />
         </div>
     </div>
 </template>
@@ -34,5 +39,9 @@ export default {
 </script>
 
 <style>
-
+    .stats {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
 </style>
